1a5466d97d02c9038bbd17fe89a6a9e60ef09398
owillard <oliviawillard04@gmail.com>
2024-11-21 12:10:28 -0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/* Mobile View */
-
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function() { 
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
